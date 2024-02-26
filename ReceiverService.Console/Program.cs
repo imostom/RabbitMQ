b993f75486ab3993447a333462a9729cacaac9ce
aytommy <oluwatomiwo@gmail.com>
2024-02-26 18:15:01 +0100

    Add project files.

---
+++ ReceiverService.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Consumer/Receiver \n");
+
+
+var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.\n");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+
+    Console.WriteLine(" [x] Done");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+channel.BasicConsume(queue: "hello",
+                     autoAck: false,
+                     consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ SenderService.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Publisher/Sender \n");
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+//var message = GetMessage(args);
+//var body = Encoding.UTF8.GetBytes(message);
+//channel.BasicPublish(exchange: string.Empty,
+//                 routingKey: "hello",
+//                 basicProperties: null,
+//                 body: body);
+//Console.WriteLine($" [x] Sent {message}");
+//Console.ReadKey();
+
+#region Basic Hello World
+Console.WriteLine("Enter message to publish/send: ");
+while (1 > 0)
+{
+
+    var message = Console.ReadLine();
+    var body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body);
+    Console.WriteLine($" [x] Sent {message}");
+}
+#endregion
+
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+}
